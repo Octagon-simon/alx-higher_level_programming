3badd827ea314f99078c1d9550adc0a9a35b6f59
Octagon-simon <ugorji757@gmail.com>
2022-02-01 03:59:15 -0800

    convert a database to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,5 @@
 -- convert a database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE  utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 COLLATE  utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY name CHARACTER SET utf8mb4 COLLATE  utf8mb4_unicod\
+e_ci;
