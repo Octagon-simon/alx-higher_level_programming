983661ca1ff1cfba88b45e7c79dfdf581e6a4fd1
Octagon-simon <ugorji757@gmail.com>
2022-02-01 03:20:26 -0800

    updated top_Score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,1 @@
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT second_table.score, second_table.name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
-CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table (id, name, score) VALUES ('1', 'John', '10');
 INSERT INTO second_table (id, name, score) VALUES ('2', 'Alex', '3');
